63ecffd566c316a1c67fa4742100e02d031ea2c7
IREMY250 <ibyishakajeanremy0@gmail.com>
2024-09-02 19:29:58 +0200

    djangowork

---
+++ advanced_features_and_security/LibraryProject/query_samples.py
@@ -0,0 +1,5 @@
+#SELECT * FROM books WHERE author = 'Example';
+#  
+Library.objects.get(name=Library_name)", "books.all()
+Author.objects.get(name=author_name)", "objects.filter(author=author)
+"Librarian.objects.get(library="
