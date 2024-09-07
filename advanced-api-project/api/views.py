56ca7ff110a56df6a7bc7e87d0cf95f999553605
IREMY250 <ibyishakajeanremy0@gmail.com>
2024-09-07 20:50:58 +0200

    advanced_api_project

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+class BookListView(generics.ListCreateAPIView):
+    """
+    List all books or create a new book.
+    GET: Retrieve a list of books.
+    POST: Create a new book.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, update, or delete a book instance.
+    GET: Retrieve a book by ID.
+    PUT: Update a book by ID.
+    DELETE: Delete a book by ID.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
