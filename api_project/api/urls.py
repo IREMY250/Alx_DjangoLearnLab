aa183db3f3fe4b499bf3d3defd8db774b9c9538d
IREMY250 <ibyishakajeanremy0@gmail.com>
2024-09-09 17:05:04 +0200

    api proj

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,14 +1,25 @@
 #api/urls.py
-from django.urls import path
-from . import views
-urlpatterns =[
+#from django.urls import path
+#from . import views
+#urlpatterns =[
     #list and detail views (already existing)
-    path('books/', views.BooklistView.as_view(), name='book-list'),
-    path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
+    #path('books/', views.BooklistView.as_view(), name='book-list'),
+    #path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
 
      #create, update ,detele views
 
-    path('books/create/', views.BookCreateView.as_view(), name='book-create'),
-    path('books/update/', views.BookUpdateView.as_view(), name='book-update'),
-    path('books/delete', views.BookDeleteView.as_view(), name='book-delete'),
+    #path('books/create/', views.BookCreateView.as_view(), name='book-create'),
+    #path('books/update/', views.BookUpdateView.as_view(), name='book-update'),
+    #path('books/delete', views.BookDeleteView.as_view(), name='book-delete'),
+#]
+
+from django.urls import path
+from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
+
+urlpatterns = [
+    path('books/', BookListView.as_view(), name='book-list'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('books/create/', BookCreateView.as_view(), name='book-create'),
+    path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book-update'),
+    path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='book-delete'),
 ]
