d3eb4f081fbc58524a01cf5b0c008523bf6d0123
IREMY250 <ibyishakajeanremy0@gmail.com>
2024-09-12 00:54:28 +0200

    api proj

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from .views import CustomBookCreateView, CustomBookListView, CustomBookDetailView, CustomBookUpdateView, CustomBookDeleteView
 
 urlpatterns = [
-    "books/delete"
-    "books/update"
+    #list and detail views (already existing)
     path('books/', CustomBookListView.as_view(), name='list'),
     path('books/<int:pk>', CustomBookDetailView.as_view(), name='detail'),
+
+    #create, update, and delete views
     path('books/update/<int:pk>', CustomBookUpdateView.as_view(), name='update'),
     path('books/delete/<int:pk>', CustomBookDeleteView.as_view(), name='delete'),
     path('books/create', CustomBookCreateView.as_view(), name='create'),
