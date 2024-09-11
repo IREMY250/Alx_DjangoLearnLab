3f273eeec1ae55898ea43eb765b4dcdef868017d
IREMY250 <ibyishakajeanremy0@gmail.com>
2024-09-12 00:35:04 +0200

    api proj

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,13 +1,13 @@
-from django_filters import rest_framework
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
-from .seriealizers import BookSerializer
+from .serializers import BookSerializer
 
 class CustomBookListView(generics.ListAPIView):
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-    filter_backends = [rest_framework.DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['title', 'author',]
     ordering_fields = ['title', 'publication_year']
 
@@ -24,9 +24,6 @@ class CustomBookListView(generics.ListAPIView):
         if publication_year:
             queryset = queryset.filter(publication_year__icontains=publication_year)
         return queryset
-
-
-
 
 class CustomBookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
@@ -47,3 +44,4 @@ class CustomBookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
+#djangowork
