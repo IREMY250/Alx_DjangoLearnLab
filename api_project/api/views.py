118211be622b678334dba8acdac227e5398f9816
IREMY250 <ibyishakajeanremy0@gmail.com>
2024-09-12 01:25:09 +0200

    api urls

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,48 +1,45 @@
-from django_filters import rest_framework
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import generics, filters
+from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic import DetailView
+from django.views.generic import CreateView
+from django.views.generic import UpdateView
+from django.views.generic import DeleteView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django.urls import reverse_lazy
 from .models import Book
-from .serializers import BookSerializer
+from django_filters import rest_framework
+from rest_framework import generics
 
-class CustomBookListView(generics.ListAPIView):
-    serializer_class = BookSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
-    search_fields = ['title', 'author',]
-    ordering_fields = ['title', 'publication_year']
-
-    def get_queryset(self):
-        queryset = Book.objects.all()
-        title = self.request.query_params.get('title')
-        author = self.request.query_params.get('author')
-        publication_year = self.request.query_params.get('publication_year')
-
-        if title:
-            queryset = queryset.filter(title__icontains=title)
-        if author:
-            queryset = queryset.filter(author__icontains=author)
-        if publication_year:
-            queryset = queryset.filter(publication_year__icontains=publication_year)
-        return queryset
-
-class CustomBookDetailView(generics.RetrieveAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+class BookListView(ListView):
+    model = Book
+    template_name = 'bookshelf/book_list.html'  # Define your template
+    context_object_name = 'books'
     permission_classes = [IsAuthenticatedOrReadOnly]
 
-class CustomBookCreateView(generics.CreateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'bookshelf/book_detail.html'  # Template for displaying a single book
+    context_object_name = 'book'
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+class BookCreateView(CreateView):
+    model = Book
+    fields = ['title', 'author', 'publication_year']  # Fields to show in the form
+    template_name = 'bookshelf/book_form.html'  # Template for creating a book
+    success_url = reverse_lazy('book-list')
     permission_classes = [IsAuthenticated]
 
-class CustomBookUpdateView(generics.UpdateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+class BookUpdateView(UpdateView):
+    model = Book
+    fields = ['title', 'author', 'publication_year']  # Fields to edit
+    template_name = 'bookshelf/book_form.html'  # Template for updating a book
+    success_url = reverse_lazy('book-list') 
     permission_classes = [IsAuthenticated]
 
-class CustomBookDeleteView(generics.DestroyAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+class BookDeleteView(DeleteView):
+    model = Book
+    template_name = 'bookshelf/book_confirm_delete.html'  # Template for confirming deletion
+    success_url = reverse_lazy('book-list') 
     permission_classes = [IsAuthenticated]
-#djangowork
