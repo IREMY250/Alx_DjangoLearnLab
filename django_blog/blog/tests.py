78ef68b1eb7dc729fa00e5905c6f8c7dba53ae25
IREMY250 <ibyishakajeanremy0@gmail.com>
2024-09-16 17:12:59 +0200

    blog proj

--- django_blog/blog/tests.py
+++ django_blog/blog/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+# blog/tests.py
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+class AuthenticationTests(TestCase):
+    def test_registration_view(self):
+        response = self.client.get(reverse('blog:register'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/register.html')
+    
+    def test_login_view(self):
+        response = self.client.get(reverse('blog:login'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/login.html')
