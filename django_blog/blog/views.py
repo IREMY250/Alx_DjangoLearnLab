a5249a45b73a00c30e9ae1f41e7a8160f3633309
IREMY250 <ibyishakajeanremy0@gmail.com>
2024-09-16 17:22:55 +0200

    blog proj

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
 
-# Create your views here.
+
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm
+from django.contrib.auth.decorators import login_required
+
+# Login View
+class CustomLoginView(LoginView):
+    template_name = 'blog/login.html'
+
+# Logout View
+class CustomLogoutView(LogoutView):
+    next_page = reverse_lazy('blog:login')
+
+# Registration View
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blog:login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+# Profile View (for authenticated users)
+@login_required
+def profile(request):
+    return render(request, 'blog/profile.html')
