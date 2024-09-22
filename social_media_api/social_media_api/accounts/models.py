0df682f4453e48005dadb7cf787584f13007239c
IREMY250 <ibyishakajeanremy0@gmail.com>
2024-09-22 15:16:55 +0200

    models.py

--- social_media_api/social_media_api/accounts/models.py
+++ social_media_api/social_media_api/accounts/models.py
@@ -1,35 +1,12 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+# accounts/models.py
 
-class CustomUserManager(BaseUserManager):
-    def create_user(self, username, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError('Email is required')
-        email = self.normalize_email(email)
-        user = self.model(
-            username=username,
-            email=email,
-            **extra_fields
-        )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-    
-    def create_superuser(self, username, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        return self.create_user(username, email, password, **extra_fields)
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
-    bio = models.TextField(blank=True)
+    bio = models.TextField(blank=True, null=True)
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
-    following = models.ManyToManyField('self', symmetrical=False, related_name='followers')
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
-
-    objects = CustomUserManager()
+    followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
 
     def __str__(self):
-        return self.email
+        return self.username
